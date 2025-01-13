16e733ef26f72caf05c7133382cd7b918d67aa3c
yepez rafael <xander21yl@gmail.com>
2025-01-13 09:15:28 -0500

    primerCommit

---
+++ for/ej1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+main(){
+    for(int i=1; i<=10; i++){
+        cout<<i<<" ";
+    }
+    cout<<"\n";
+      for(int i=2; i<=10; i+=2){
+        cout<<i<<" ";
+    }  
+     cout<<"\n";
+      for(int i=10; i>=1; i--){
+        cout<<i<<" ";
+    }
+    cout<<"\n";
+      for(int i=10; i>=1; i-=2){
+        cout<<i<<" ";
+    }
+
+}
